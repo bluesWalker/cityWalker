ab8b95a8cab751e1b6f75bfe9b6b9f636a99d6fd
bluesWalker <live4ever00@outlook.com>
2014-07-28 23:54:29 +0100

    Initial Commit

---
+++ cityWalkerTests/cityWalkerTests.m
@@ -0,0 +1,34 @@
+//
+//  cityWalkerTests.m
+//  cityWalkerTests
+//
+//  Created by bluesWalker on 7/28/14.
+//  Copyright (c) 2014 Newcastle University. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface cityWalkerTests : XCTestCase
+
+@end
+
+@implementation cityWalkerTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
